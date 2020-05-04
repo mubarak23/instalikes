9b90573cb0b21b1cc8282765d38ec729f81151d9
mubarak23 <mubarakaminu340@gmail.com>
2020-05-04 13:39:48 +0100

    setup navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1>This is instaLkie</h1>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div class='nav-wrapper'>
+        <a href='#' className='brand-logo'>
+          Logo
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <a href='sass.html'>Sass</a>
+          </li>
+          <li>
+            <a href='badges.html'>Components</a>
+          </li>
+          <li>
+            <a href='collapsible.html'>JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
