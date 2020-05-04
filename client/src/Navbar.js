8df7be6562b1eccfea4ebfba70bbc8c5b94327d9
mubarak23 <mubarakaminu340@gmail.com>
2020-05-04 13:33:43 +0100

    setting up react client

---
+++ client/src/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+  <nav>
+    <div class='nav-wrapper'>
+      <a href='#' className='brand-logo'>
+        Logo
+      </a>
+      <ul id='nav-mobile' className='right hide-on-med-and-down'>
+        <li>
+          <a href='sass.html'>Sass</a>
+        </li>
+        <li>
+          <a href='badges.html'>Components</a>
+        </li>
+        <li>
+          <a href='collapsible.html'>JavaScript</a>
+        </li>
+      </ul>
+    </div>
+  </nav>;
+};
