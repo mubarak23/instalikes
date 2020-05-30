e2d92c38cc6c72796f86937c0d08caf7341750a7
mubarak23 <mubarakaminu340@gmail.com>
2020-05-30 13:26:15 +0100

    add ui for like and unlike post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { userContext } from '../../App';
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const { state, dispatch } = useContext(userContext);
   useEffect(() => {
     fetch('/allpost', {
       headers: {
@@ -14,6 +16,9 @@ const Home = () => {
         console.log(result.posts);
       });
   }, []);
+
+  const likePost = (postid) => {};
+  const unlikePost = (postid) => {};
   return (
     <div className='home'>
       {data.map((item) => {
@@ -25,6 +30,26 @@ const Home = () => {
             </div>
             <div className='card-content'>
               <i className='material-icons'>favorite</i>
+              {item.likes.includes(state._id) ? (
+                <i
+                  className='material-icons'
+                  onClick={() => {
+                    unlikePost(item._id);
+                  }}
+                >
+                  thumb_down
+                </i>
+              ) : (
+                <i
+                  className='material-icons'
+                  onClick={() => {
+                    likePost(item._id);
+                  }}
+                >
+                  thumb_up
+                </i>
+              )}
+
               <h5>{item.title}</h5>
               <p>{item.body}</p>
               <input type='text' placeholder='Add a comment' />
